21d95a7667b6fc5b643157d0f37f6a4f5d752821
ika1511 <bhavikausadm@gmail.com>
2025-05-29 18:58:22 -0400

    Update YESNNO.py

--- YESNNO.py
+++ YESNNO.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1uE3DeahiiCP_yfHVuSOTLgDHJgDMnT5C
 """
 
-!pip install streamlit
 !pip install boto3 
 !pip install pdf2image
 !apt-get install -y poppler-utils
